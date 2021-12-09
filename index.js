1cf742ed3df53440ca585896c8682180db2a50d5
Mickman <josh.abramoff@rutgers.edu>
2021-12-09 02:16:56 -0500

    init commit

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-let { Client, Intents } = require('discord.js')
-let client = new Client({ intents: [Intents.FLAGS.GUILDS] })
-const { token } = require('./config.json')
+const fs = require('fs')
+const { Client, Collection, Intents } = require('discord.js')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+const { token } = require('./config/config.json')
 
-client.login(token)
+client.commands = new Collection()
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`)
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command)
+}
+
 client.on('ready', () => {
 	console.log('Ready!')
-	
-	client.guilds.cache.forEach(guild => { console.log(guild.name) })
 })
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+})
+
+client.login(token)
