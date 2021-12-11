16308affc2feaf652d01c46b6a0a865638e4a1c9
Mickman <josh.abramoff@rutgers.edu>
2021-12-10 20:25:21 -0500

    user guesses with matching responses
    - colored embeds to match outcome
    - guesses are optional

--- commands/coin.js
+++ commands/coin.js
@@ -9,12 +9,12 @@ module.exports = {
 				.setDescription('Guess heads or tails')
 				.setRequired(false)
 				.addChoice('Heads', 'guess_heads')
-				.addChoice('Tails', 'guess_tails')),
+				.addChoice('Tails', 'guess_tails')
+		),
 		
 	async execute(interaction) {
 		let { makeEmbed } = require('../config/functions.js')
 
-		console.log(interaction.options.data)
 		const { MessageEmbed } = require('discord.js')
 		const embed = new MessageEmbed()
 
@@ -22,18 +22,31 @@ module.exports = {
 			'https://cdn.discordapp.com/attachments/596928630009888781/650328670724161547/CoinFlipHeadsFinal.gif',
 			'https://cdn.discordapp.com/attachments/596928630009888781/779502238590566440/CoinFlipTailsFinal.gif'
 		]
-		
+
 		let randomNum = Math.floor(Math.random() * 2)
 
 		embed.setImage(coinGIF[randomNum])
-		embed.setColor('#3498DB')
+		embed.setColor(MICKBOT_BLUE)
 
 		await interaction.reply({ embeds: [embed] })
 
 		setTimeout(() => {
-      interaction.channel.send({ embeds: [makeEmbed(randomNum === 0 ? '**Heads!**' : '**Tails!**', MICKBOT_BLUE)] })
-    }, 1900)
-		
-		//await interaction.channel.send('Correct!')
+			let coinOutcomeEmbedColor = MICKBOT_BLUE
+			let guessOutcome = ''
+			// Check if user gave an optional guess
+			if (interaction.options.data.length > 0) {
+				// Check if user's guess matches outcome, set embed color accordingly
+				if ((interaction.options.data[0].value === 'guess_heads' && randomNum === 0) || (interaction.options.data[0].value === 'guess_tails' && randomNum === 1)) {
+					guessOutcome = '\nYou guessed correctly!'
+					coinOutcomeEmbedColor = MICKBOT_GREEN
+				}
+				else {
+					guessOutcome = '\nYou guessed incorrectly!'
+					coinOutcomeEmbedColor = MICKBOT_RED
+				}
+			}
+
+			interaction.channel.send({ embeds: [makeEmbed((randomNum === 0 ? '**Heads!**' : '**Tails!**') + guessOutcome, coinOutcomeEmbedColor)] })
+		}, 1900)
 	},
 }
