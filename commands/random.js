99a244f392405bd8bffa1363bfd9b14bdfe0711a
Mickman <josh.abramoff@rutgers.edu>
2021-12-10 22:28:55 -0500

    Create random.js

---
+++ commands/random.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('random')
+		.setDescription('Picks a random number between <min> and <max>')
+		.addIntegerOption(option =>
+			option.setName('min')
+				.setDescription('The minimum number')
+				.setRequired(true)
+		)
+		.addIntegerOption(option =>
+			option.setName('max')
+				.setDescription('The maximum number')
+				.setRequired(false)
+		),
+		
+	async execute(interaction) {
+		const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js')
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('primary')
+					.setLabel('Re-Roll?')
+					.setStyle('PRIMARY'),
+			)
+
+		let min, max
+
+		min = interaction.options.data[0].value
+
+		if (interaction.options.data.length > 1) 
+			max = interaction.options.data[1].value
+
+		console.log(min)
+		console.log(max)
+
+		const randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+
+		await interaction.reply({ embeds: [ makeEmbed(randomNum.toString(), MICKBOT_BLUE) ], components: [row] });
+	},
+}
