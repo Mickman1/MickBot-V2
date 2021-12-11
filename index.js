d49e65ed160b38c446f8e0bf07e13f02066fef4a
Mickman <josh.abramoff@rutgers.edu>
2021-12-10 19:08:35 -0500

    added MICKBOT colors

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const fs = require('fs')
 const { Client, Collection, Intents } = require('discord.js')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 const { token } = require('./config/config.json')
+MICKBOT_BLUE = '#3498DB'
+MICKBOT_RED = '#DB3434'
+MICKBOT_ORANGE = '#DB6E34'
 
 client.commands = new Collection()
 
@@ -19,17 +22,19 @@ client.on('ready', () => {
 })
 
 client.on('interactionCreate', async interaction => {
+	//console.log(interaction)
+	//interaction.reply('Hello!')
 	if (!interaction.isCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
+	const command = client.commands.get(interaction.commandName)
 
 	if (!command) return;
 
 	try {
-		await command.execute(interaction);
+		await command.execute(interaction)
 	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		console.error(error)
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
 	}
 })
 
