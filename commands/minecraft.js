7b4f60e0eae7e57eb771e3cd92e565f182631f8d
Mickman <josh.abramoff@rutgers.edu>
2021-12-13 04:19:07 -0500

    set your skin option
    - changed some embed formatting

--- commands/minecraft.js
+++ commands/minecraft.js
@@ -24,14 +24,16 @@ module.exports = {
 		const { MessageEmbed } = require('discord.js')
 		const embed = new MessageEmbed()
 
+		let username = interaction.options.data[0].value
 		let avatarType = 'body'
 		// Check if user gave an optional avatar-type
 		if (interaction.options.data.length > 1) {
 			avatarType = interaction.options.data[1].value
 		}
 
-		embed.setImage(`https://mc-heads.net/${avatarType}/${interaction.options.data[0].value}/1000`)
-		embed.setAuthor(interaction.options.data[0].value, 'https://media.discordapp.net/attachments/596928630009888781/786054261787983913/grass.png')
+		embed.setImage(`https://mc-heads.net/${avatarType}/${username}/400`)
+		embed.setAuthor('Set as your skin?', 'https://media.discordapp.net/attachments/596928630009888781/786054261787983913/grass.png', `https://mc-heads.net/change/${username}`)
+		embed.setTitle(username)
 		embed.setColor(MICKBOT_BLUE)
 
 		await interaction.reply({ embeds: [ embed ] })
