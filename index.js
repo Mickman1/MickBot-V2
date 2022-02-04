e9f0e29052795b2196e8b36c2f22cc9cb014ce8c
Mickman <josh.abramoff@rutgers.edu>
2022-02-04 05:31:28 -0500

    fixed spacing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const { Client, Collection, Intents } = require('discord.js')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+const client = new Client({ intents: [ Intents.FLAGS.GUILDS ] })
 const { token } = require('./config/config.json')
 
 // Global MickBot colors
