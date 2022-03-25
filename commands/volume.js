5b318e159b19908a69797c7fb011501c00fe143a
Mickman <josh.abramoff@rutgers.edu>
2022-03-25 19:51:54 -0400

    volume init commit
    - use /volume to show current volume
    - /volume <number> to change it
    - currently no input validation for volumes >100 or <0

---
+++ commands/volume.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+//const { createAudioPlayer, createAudioResource, joinVoiceChannel, VoiceConnectionStatus, AudioPlayerStatus } = require('@discordjs/voice')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('volume')
+		.setDescription('Show or change the current volume')
+		.addIntegerOption(option =>
+			option.setName('volume')
+				.setDescription('Volume to use')
+				.setRequired(false)
+		),
+		
+	async execute(interaction) {
+		const { guildId } = interaction
+		const { queues } = interaction.client
+		const queue = queues.get(guildId)
+
+		const currentVolume = queue.resource.volume.volume
+
+		// If user supplied a new volume value
+		if (interaction.options.data.length > 0) {
+			// Format volume between 0 and 1 (ex: 100 -> 1, 47 -> 0.47)
+			const newVolume = interaction.options.getInteger('volume') / 100
+
+			// Set resource / prism-media volume, and update the queue object's volume to set the temporary default
+			queue.resource.volume.setVolume(newVolume)
+			queue.volume = newVolume
+
+			const formattedVolume = `${newVolume * 100}%`
+			return await interaction.reply(`Volume changed to \`${formattedVolume}\`!`);
+		}
+
+		const formattedVolume = `${currentVolume * 100}%`
+		await interaction.reply(`Volume is \`${formattedVolume}\`!`)
+	},
+}
