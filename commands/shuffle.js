7924faee5dbb060468498eb63e9f56bf6369530e
Mickman <josh.abramoff@rutgers.edu>
2022-03-28 12:23:06 -0400

    Create shuffle.js

---
+++ commands/shuffle.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+//const { createAudioPlayer, createAudioResource, joinVoiceChannel, VoiceConnectionStatus, AudioPlayerStatus } = require('@discordjs/voice')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('shuffle')
+		.setDescription('Shuffle the queue'),
+		
+	async execute(interaction) {
+		const { guildId } = interaction
+		const { queues } = interaction.client
+		const queue = queues.get(guildId)
+		const array = queue.songs
+		const currentSong = queue.songs[queue.head]
+
+		queue.songs.splice(queue.head, 1)
+
+		var currentIndex = array.length
+		var temporaryValue
+		var randomIndex
+
+		// While there remain elements to shuffle...
+		while (0 !== currentIndex) {
+			// Pick a remaining element...
+			randomIndex = Math.floor(Math.random() * currentIndex)
+			currentIndex -= 1
+	
+			// And swap it with the current element.
+			temporaryValue = array[currentIndex]
+			array[currentIndex] = array[randomIndex]
+			array[randomIndex] = temporaryValue
+		}
+
+		queue.songs.splice(queue.head, 0, currentSong)
+
+		await interaction.reply('Shuffled queue!')
+	},
+}
