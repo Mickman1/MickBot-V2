e6375d12d98d24f2ae660b3e254e6e98c556d8c0
Mickman <josh.abramoff@rutgers.edu>
2022-04-01 03:42:30 -0400

    clear command init commit

---
+++ commands/clear.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+//const { createAudioPlayer, createAudioResource, joinVoiceChannel, VoiceConnectionStatus, AudioPlayerStatus } = require('@discordjs/voice')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('clear')
+		.setDescription('Clear the current queue'),
+		
+	async execute(interaction) {
+		const { guildId } = interaction
+		const { queues } = interaction.client
+		const queue = queues.get(guildId)
+
+		const currentSong = queue.songs[queue.head]
+
+		queue.songs = []
+		queue.songs.push(currentSong)
+
+		await interaction.reply('Queue cleared!')
+	},
+}
