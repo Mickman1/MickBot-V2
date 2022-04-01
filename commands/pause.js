0dd5f198f3530218150fe572cfb0b2c3df278e3d
Mickman <josh.abramoff@rutgers.edu>
2022-04-01 08:01:52 -0400

    pause & unpause init commit

---
+++ commands/pause.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+//const { createAudioPlayer, createAudioResource, joinVoiceChannel, VoiceConnectionStatus, AudioPlayerStatus } = require('@discordjs/voice')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('pause')
+		.setDescription('Pause current track'),
+		
+	async execute(interaction) {
+		const { guildId } = interaction
+		const { queues } = interaction.client
+		const queue = queues.get(guildId)
+
+		queue.player.pause()
+
+		await interaction.reply('Paused!')
+	},
+}
---
+++ commands/unpause.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+//const { createAudioPlayer, createAudioResource, joinVoiceChannel, VoiceConnectionStatus, AudioPlayerStatus } = require('@discordjs/voice')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('unpause')
+		.setDescription('Unpause current track'),
+		
+	async execute(interaction) {
+		const { guildId } = interaction
+		const { queues } = interaction.client
+		const queue = queues.get(guildId)
+
+		queue.player.unpause()
+
+		await interaction.reply('Unpaused!')
+	},
+}
