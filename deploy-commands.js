4a34ea24da0a2b5ae35844fafb73e7293a95e0ab
Mickman <70730433+Mickman1@users.noreply.github.com>
2025-01-26 00:23:51 -0500

    commander for custom options / flags
    - --guild to set custom guild, different from config.json
    - --global to register global application commands
    - --clear to clear commands from either global or guild

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,19 +1,43 @@
+const { Command } = require('commander')
+const program = new Command()
+program
+	.name('node deploy-commands')
+	.description('Deploy application commands to Discord API')
+	.option('-g, --guild <Guild ID>', 'Set guild for guild command registration')
+	.option('-G, --global', 'Register commands globally')
+	.option('-c, --clear', 'Clear all commands from the application')
+	.helpOption('-h, --help', 'Show help information')
+	.parse()
+const options = program.opts()
+
 const fs = require('fs')
 const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
 const { clientId, guildId, token } = require('./config/config.json')
 
-const commands = []
+const deploymentType = options.global ? 'global' : 'guild'
+const deploymentGuildId = options.guild || guildId
+let commands = []
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`)
-	commands.push(command.data.toJSON())
+if (!options.clear) {
+	const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+	
+	for (const file of commandFiles) {
+		const command = require(`./commands/${file}`)
+		commands.push(command.data.toJSON())
+	}
 }
 
 const rest = new REST({ version: '9' }).setToken(token)
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error)
+if (deploymentType === 'guild') {
+	rest.put(Routes.applicationGuildCommands(clientId, deploymentGuildId), { body: commands })
+		.then(() => console.log('Successfully registered guild application commands.'))
+		.catch(console.error)
+}
+
+if (deploymentType === 'global') {
+	rest.put(Routes.applicationCommands(clientId), { body: commands })
+		.then(() => console.log('Successfully registered global application commands.'))
+		.catch(console.error)
+}
