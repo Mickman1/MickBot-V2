8b9aed65448d489f5375ebb43f5eb3cc9aa1cc6e
Mickman <70730433+Mickman1@users.noreply.github.com>
2025-01-30 10:06:36 -0500

    reload command

---
+++ commands/reload.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('reload')
+		.setDescription('Reload a command')
+		.addStringOption(option =>
+			option.setName('command')
+				.setDescription('The command to reload')
+				.setRequired(true)
+		),
+
+	async execute(interaction) {
+		const commandName = interaction.options.getString('command', true).toLowerCase()
+		const command = interaction.client.commands.get(commandName)
+
+		if (!command) {
+			return interaction.reply(`There is no command with name \`${commandName}\`!`);
+		}
+
+		delete require.cache[require.resolve(`../commands/${command.data.name}.js`)]
+
+		try {
+			const newCommand = require(`../commands/${command.data.name}.js`)
+			interaction.client.commands.set(newCommand.data.name, newCommand)
+
+			const embed = new EmbedBuilder()
+				.setColor(MICKBOT_BLUE)
+				.setDescription(`Command \`${newCommand.data.name}\` was reloaded!`)
+
+			await interaction.reply({ embeds: [embed] })
+		}
+		catch (error) {
+			console.error(error)
+			await interaction.reply(`There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``)
+		}
+	},
+}
