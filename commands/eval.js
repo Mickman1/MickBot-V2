d28c665e4fffcf2a9a50727546933d34e111be10
Mickman <70730433+Mickman1@users.noreply.github.com>
2025-02-03 06:37:08 -0500

    optional string for single-line expression

--- commands/eval.js
+++ commands/eval.js
@@ -4,7 +4,11 @@ const { adminUserIds } = require('../config/config.json')
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('eval')
-		.setDescription('Evaluate JavaScript'),
+		.setDescription('Evaluate JavaScript')
+		.addStringOption(option =>
+			option.setName('expression')
+				.setDescription('The expression to evaluate')
+		),
 
 	async execute(interaction) {
 		if (!adminUserIds.includes(interaction.user.id)) {
@@ -12,6 +16,23 @@ module.exports = {
 				.setColor(MICKBOT_RED)
 				.setDescription(`You don't have permissions to use this command!`)
 			await interaction.reply({ embeds: [embed] })
+			return;
+		}
+
+		if (interaction.options.data.length !== 0) {
+			const expression = interaction.options.getString('expression')
+
+			const evaluatedExpression = eval(expression)
+
+			const inputEmbed = new EmbedBuilder()
+				.setColor(MICKBOT_BLUE)
+				.setDescription(`\`\`\`js\n${expression}\n\`\`\``)
+			await interaction.reply({ embeds: [inputEmbed] })
+
+			const outputEmbed = new EmbedBuilder()
+				.setColor(MICKBOT_BLUE)
+				.setDescription(`\`\`\`js\n${evaluatedExpression}\n\`\`\``)
+			await interaction.followUp({ embeds: [outputEmbed] })
 			return;
 		}
 
