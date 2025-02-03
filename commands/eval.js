70b67023bb35c3a9cec41ec69dc2b7449d421111
Mickman <70730433+Mickman1@users.noreply.github.com>
2025-02-03 01:26:01 -0500

    eval command
    - using basic user prevention, need to use built-in slash command gating in the future

---
+++ commands/eval.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const { adminUserId } = require('../config/config.json')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('eval')
+		.setDescription('Evaluate JavaScript')
+		.addStringOption(option =>
+			option.setName('expression')
+				.setDescription('The expression to evaluate')
+				.setRequired(true)
+		),
+
+	async execute(interaction) {
+		if (interaction.user.id !== adminUserId) {
+			const embed = new EmbedBuilder()
+				.setColor(MICKBOT_RED)
+				.setDescription(`You don't have permissions to use this command!`)
+			await interaction.reply({ embeds: [embed] })
+			return;
+		}
+
+		const expression = interaction.options.getString('expression')
+
+		try {
+			const evaluatedExpression = eval(expression)
+
+			const embed = new EmbedBuilder()
+				.setColor(MICKBOT_BLUE)
+				.setDescription(`\`\`\`js\n${evaluatedExpression}\n\`\`\``)
+			await interaction.reply({ embeds: [embed] })
+		}
+		catch (error) {
+			print('Eval error', 'red', '❗')
+			console.log(error)
+
+			const embed = new EmbedBuilder()
+				.setColor(MICKBOT_RED)
+				.setDescription('Eval error!')
+			await interaction.reply({ embeds: [embed] })
+		}
+	},
+}
