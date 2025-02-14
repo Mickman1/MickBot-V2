e6061ae455d4e2106ccb1cff82912cffc5a9c07c
Mickman <70730433+Mickman1@users.noreply.github.com>
2025-02-14 02:57:35 -0500

    banner command

---
+++ commands/banner.js
@@ -0,0 +1,56 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const ColorThief = require('colorthief')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('banner')
+		.setDescription('Grab your banner, or the banner of a mentioned user')
+		.addUserOption(option =>
+			option.setName('user')
+				.setDescription('Who\'s banner to send')
+				.setRequired(false)
+		)
+		.setIntegrationTypes(0, 1)
+		.setContexts(0, 1, 2),
+
+	async execute(interaction) {
+		// "targetUser" is whoever's banner to grab
+		// If no mentioned user in the options, it's who sent the command
+		let targetUser = await interaction.user.fetch()
+
+		// Check for optional mentioned user
+		if (interaction.options.data.length > 0) {
+			targetUser = await interaction.options.getUser('user').fetch()
+		}
+
+		if (targetUser.banner === null) {
+			const embed = new EmbedBuilder()
+				.setDescription('This user has no banner!')
+				.setColor(MICKBOT_RED)
+
+			interaction.reply({ embeds: [embed] })
+			return;
+		}
+
+		// Grab banner URL. Prefer .png, but could return .gif
+		let embedBannerURL = await targetUser.bannerURL({ extension: 'webp', size: 4096 })
+
+		// If targetUser doesn't have an banner set, use their default Discord banner
+		if (targetUser.banner === null) {
+			embedBannerURL = targetUser.defaultAvatarURL
+		}
+
+		const bannerColor = await ColorThief.getColor(embedBannerURL)
+
+		const embed = new EmbedBuilder()
+			.setAuthor({
+				name: targetUser.username,
+				url: `https://discord.com/users/${targetUser.id}`,
+			})
+			.setImage(embedBannerURL)
+			.setTitle(targetUser.displayName)
+			.setColor(bannerColor)
+
+		interaction.reply({ embeds: [embed] })
+	},
+}
