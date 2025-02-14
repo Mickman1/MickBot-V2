7f82d7582118eda81d26205eb1a5b2481ce574d4
Mickman <70730433+Mickman1@users.noreply.github.com>
2025-02-14 18:57:47 -0500

    server banners, dm compat., error handling
    - added choices for profile type. can now send user profile or server profile
    - get user profile first, if no banner, fall back to server banner if there is one. and vice versa for if profile-type was supplied with "server", then fall back to user profile.
    - dmBanner() for running in DMs or group chats. doesn't make use of getMember or the profile-type option. is there a way to remove the option altogether for DM contexts?

--- commands/banner.js
+++ commands/banner.js
@@ -10,22 +10,92 @@ module.exports = {
 				.setDescription('Grab another user\'s banner?')
 				.setRequired(false)
 		)
+		.addStringOption(option =>
+			option.setName('profile-type')
+				.setDescription('User Profile or Server Profile? Only works in servers. (Default: User)')
+				.setRequired(false)
+				.addChoices(
+					{ name: 'User Profile', value: 'user' },
+					{ name: 'Server Profile', value: 'server' },
+				)
+		)
 		.setIntegrationTypes(0, 1)
 		.setContexts(0, 1, 2),
 
 	async execute(interaction) {
-		// "targetUser" is whoever's banner to grab
-		// If no mentioned user in the options, it's who sent the command
+		// If command was sent in Direct Messages
+		if (interaction.context === 1 || interaction.context === 2) {
+			this.dmBanner(interaction)
+			return;
+		}
+
+		// Set targetUser to the Command sender by default
+		// Then work out mentioned user and profile type
+		let targetUser = interaction.member
+
+		if (interaction.options.getUser('user')) {
+			targetUser = interaction.options.getMember('user')
+		}
+
+		// If no selected profile type (default: user), or selected 'User Profile' option, targetUser becomes .user object
+		if (!interaction.options.getString('profile-type') || interaction.options.getString('profile-type') === 'user') {
+			const user = await targetUser.user.fetch()
+			if (user.banner) {
+				targetUser = user
+			}
+		}
+
+		// Fallback to Server Profile if available
+		if (!targetUser.banner) {
+			targetUser = targetUser.user
+
+			// If still nothing, error
+			if (!targetUser.banner) {
+				const embed = new EmbedBuilder()
+					.setDescription('This user has no banner!')
+					.setColor(MICKBOT_RED)
+
+				interaction.reply({ embeds: [embed] })
+				return;
+			}
+		}
+
+		// Grab banner URL. Prefer .png, but could return .gif
+		const embedBannerURL = await targetUser.bannerURL({ extension: 'webp', size: 4096 })
+
+		const bannerColor = await ColorThief.getColor(embedBannerURL)
+
+		const embed = new EmbedBuilder()
+			.setAuthor({
+				name: targetUser.username || targetUser.user.username,
+				url: `https://discord.com/users/${targetUser.id}`,
+			})
+			.setImage(embedBannerURL)
+			.setTitle(targetUser.displayName)
+			.setColor(bannerColor)
+
+		interaction.reply({ embeds: [embed] })
+	},
+
+	async dmBanner(interaction) {
+		// Set targetUser to the Command sender by default
+		// Then work out mentioned user and profile type
 		let targetUser = await interaction.user.fetch()
 
-		// Check for optional mentioned user
-		if (interaction.options.data.length > 0) {
+		if (interaction.options.getUser('user')) {
 			targetUser = await interaction.options.getUser('user').fetch()
 		}
 
-		if (targetUser.banner === null) {
+		if (!targetUser.banner) {
+			let errorMessage = 'This user has no banner!'
+
+			if (targetUser === interaction.user)
+				errorMessage = 'You have no banner! 🫵'
+			if (targetUser === interaction.client.user)
+				errorMessage = 'I have no banner! 😔'
+
 			const embed = new EmbedBuilder()
-				.setDescription('This user has no banner!')
+				.setDescription(errorMessage)
 				.setColor(MICKBOT_RED)
 
 			interaction.reply({ embeds: [embed] })
@@ -39,7 +109,7 @@ module.exports = {
 
 		const embed = new EmbedBuilder()
 			.setAuthor({
-				name: targetUser.username,
+				name: targetUser.username || targetUser.user.username,
 				url: `https://discord.com/users/${targetUser.id}`,
 			})
 			.setImage(embedBannerURL)
