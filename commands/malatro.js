6b3c2f821a9378acde7e443e97829070b8b07f62
Mickman <70730433+Mickman1@users.noreply.github.com>
2025-02-18 19:48:30 -0500

    slash command boilerplate

--- commands/malatro.js
+++ commands/malatro.js
@@ -1,3 +1,5 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+
 class Card {
 	constructor({ rank, suit, edition = null, enhancement = null, seal = null, disabled = false }) {
 		this.rank = rank
@@ -9,46 +11,67 @@ class Card {
 	}
 }
 
-const SUITS = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('malatro')
+		.setDescription('Play Malatro!')
+		.setIntegrationTypes(0, 1)
+		.setContexts(0, 1, 2),
 
-// Initialize deck and fill with default cards
-const deck = []
-for (let i = 0; i < 4; i++) {
-	for (let j = 2; j < 15; j++) {
-		const card = new Card({
-			rank: j,
-			suit: SUITS[i],
-			edition: null,
-			enhancement: null,
-			seal: null,
-			disabled: false,
-		})
-		deck.push(card)
-	}
-}
+	async execute(interaction) {
+		const SUIT_NAMES = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
 
-let remainingCards = [...deck]
-shuffleCards(remainingCards)
-
-function drawCards(amount) {
-	const dealtCards = []
-
-	for (let i = 0; i < amount; i++) {
-		if (remainingCards.length === 0) {
-			console.log('No more cards!')
-			break
+		// Initialize deck and fill with default cards
+		const deck = []
+		for (let i = 0; i < 4; i++) {
+			for (let j = 2; j < 15; j++) {
+				const card = new Card({
+					rank: j,
+					suit: SUIT_NAMES[i],
+					edition: null,
+					enhancement: null,
+					seal: null,
+					disabled: false,
+				})
+				deck.push(card)
+			}
 		}
 
-		dealtCards.push(remainingCards[0])
-		remainingCards = remainingCards.slice(1)
-	}
+		let remainingCards = [...deck]
+		shuffleCards(remainingCards)
 
-	return dealtCards
-}
+		const hand = drawCards(8)
 
-function shuffleCards(cards) {
-	for (let i = cards.length - 1; i >= 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[cards[i], cards[j]] = [cards[j], cards[i]]
-	}
+		let embedHand = ''
+		for (let i = 0; i < hand.length; i++) {
+			embedHand += `${hand[i].rank}${hand[i].emoji} `
+		}
+		const embed = new EmbedBuilder()
+			.setDescription(embedHand)
+			.setColor(MICKBOT_BLUE)
+		interaction.reply({ embeds: [embed] })
+
+		function drawCards(amount) {
+			const dealtCards = []
+
+			for (let i = 0; i < amount; i++) {
+				if (remainingCards.length === 0) {
+					console.log('No more cards!')
+					break
+				}
+
+				dealtCards.push(remainingCards[0])
+				remainingCards = remainingCards.slice(1)
+			}
+
+			return dealtCards
+		}
+
+		function shuffleCards(cards) {
+			for (let i = cards.length - 1; i >= 0; i--) {
+				const j = Math.floor(Math.random() * (i + 1));
+				[cards[i], cards[j]] = [cards[j], cards[i]]
+			}
+		}
+	},
 }
