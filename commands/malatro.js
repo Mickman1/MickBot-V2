e070e806a9f2dd5d18324dfc324cb6ba6bb51040
Mickman <70730433+Mickman1@users.noreply.github.com>
2025-02-19 05:06:41 -0500

    rank titles, chip values

--- commands/malatro.js
+++ commands/malatro.js
@@ -1,11 +1,13 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 
 class Card {
-	constructor({ rank, suit, emoji, color, edition = null, enhancement = null, seal = null, disabled = false }) {
+	constructor({ rank, rankTitle, suit, emoji, color, chips, edition = null, enhancement = null, seal = null, disabled = false }) {
 		this.rank = rank
+		this.rankTitle = rankTitle
 		this.suit = suit
 		this.emoji = emoji
 		this.color = color
+		this.chips = chips
 		this.edition = edition
 		this.enhancement = enhancement
 		this.seal = seal
@@ -37,6 +39,8 @@ module.exports = {
 		.setContexts(0, 1, 2),
 
 	async execute(interaction) {
+		const RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+		const CHIP_VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
 		const SUIT_NAMES = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
 		const SUIT_EMOJIS = ['♠️', '♥️', '♣️', '♦️']
 		const SUIT_COLORS = ['blue', 'red', 'green', 'yellow']
@@ -44,12 +48,14 @@ module.exports = {
 		// Initialize deck and fill with default cards
 		const deck = []
 		for (let i = 0; i < 4; i++) {
-			for (let j = 2; j < 15; j++) {
+			for (let j = 0; j < 13; j++) {
 				const card = new Card({
-					rank: j,
+					rank: j + 2,
+					rankTitle: RANKS[j],
 					suit: SUIT_NAMES[i],
 					emoji: SUIT_EMOJIS[i],
 					color: SUIT_COLORS[i],
+					chips: CHIP_VALUES[j],
 					edition: null,
 					enhancement: null,
 					seal: null,
@@ -66,7 +72,7 @@ module.exports = {
 
 		let embedHand = ''
 		for (let i = 0; i < hand.length; i++) {
-			embedHand += `${hand[i].rank}${hand[i].emoji} `
+			embedHand += `${hand[i].rankTitle}${hand[i].emoji} `
 		}
 		const embed = new EmbedBuilder()
 			.setDescription(embedHand)
