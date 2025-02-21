1a3a4249c4e25f78a7482fd0b0bbaadc1184a8ce
Mickman <70730433+Mickman1@users.noreply.github.com>
2025-02-20 21:45:46 -0500

    decks list
    - need to add functionality for 'special' decks like green and plasma

---
+++ malatro/decks.js
@@ -0,0 +1,40 @@
+module.exports = {
+	red: {
+		discards: 3,
+	},
+	blue: {
+		hands: 5,
+	},
+	yellow: {
+		money: 14,
+	},
+	green: {
+	},
+	black: {
+		jokerSlots: 6,
+		hands: 3,
+	},
+	magic: {
+	},
+	nebula: {
+		consumableSlots: 1,
+	},
+	ghost: {
+	},
+	abandoned: {
+	},
+	checkered: {
+	},
+	zodiac: {
+	},
+	painted: {
+		handSize: 10,
+		jokerSlots: 4,
+	},
+	anaglyph: {
+	},
+	plasma: {
+	},
+	erratic: {
+	},
+}
