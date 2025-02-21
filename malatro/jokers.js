7156e3515435a23ecf9c1ff84950fad7d0c7ae44
Mickman <70730433+Mickman1@users.noreply.github.com>
2025-02-21 02:01:53 -0500

    cost -> baseCost
    - safe to change this to avoid duplicate names. the shop will use "cost" and the joker definitions will use "baseCost"

--- malatro/jokers.js
+++ malatro/jokers.js
@@ -1,602 +1,602 @@
 module.exports = {
 	joker: {
 		emote: '<:Joker:1341987050974351440>',
-		cost: 2,
+		baseCost: 2,
 	},
 	greedyJoker: {
 		emote: '<:Greedy_Joker:1341987332701818880>',
-		cost: 5,
+		baseCost: 5,
 	},
 	lustyJoker: {
 		emote: '<:Lusty_Joker:1341987482954240040>',
-		cost: 5,
+		baseCost: 5,
 	},
 	wrathfulJoker: {
 		emote: '<:Wrathful_Joker:1341987520539263087>',
-		cost: 5,
+		baseCost: 5,
 	},
 	gluttonousJoker: {
 		emote: '<:Gluttonous_Joker:1341987528843989154>',
-		cost: 5,
+		baseCost: 5,
 	},
 	jollyJoker: {
 		emote: '<:Jolly_Joker:1341987622867832843>',
-		cost: 3,
+		baseCost: 3,
 	},
 	zanyJoker: {
 		emote: '<:Zany_Joker:1341987680170545212>',
-		cost: 4,
+		baseCost: 4,
 	},
 	madJoker: {
 		emote: '<:Mad_Joker:1341987689280442399>',
-		cost: 4,
+		baseCost: 4,
 	},
 	crazyJoker: {
 		emote: '<:Crazy_Joker:1341987698289807451>',
-		cost: 4,
+		baseCost: 4,
 	},
 	drollJoker: {
 		emote: '<:Droll_Joker:1341987706552582266>',
-		cost: 4,
+		baseCost: 4,
 	},
 	slyJoker: {
 		emote: '<:Sly_Joker:1341987748415799337>',
-		cost: 3,
+		baseCost: 3,
 	},
 	wilyJoker: {
 		emote: '<:Wily_Joker:1341987792711843983>',
-		cost: 4,
+		baseCost: 4,
 	},
 	cleverJoker: {
 		emote: '<:Clever_Joker:1341987804263088158>',
-		cost: 4,
+		baseCost: 4,
 	},
 	deviousJoker: {
 		emote: '<:Devious_Joker:1341987812953559060>',
-		cost: 4,
+		baseCost: 4,
 	},
 	craftyJoker: {
 		emote: '<:Crafty_Joker:1341987822793527308>',
-		cost: 4,
+		baseCost: 4,
 	},
 	halfJoker: {
 		emote: '<:Half_Joker:1341987868134080575>',
-		cost: 5,
+		baseCost: 5,
 	},
 	jokerStencil: {
 		emote: '<:Joker_Stencil:1342001026294616104>',
-		cost: 8,
+		baseCost: 8,
 	},
 	fourFingers: {
 		emote: '<:Four_Fingers:1342001487529771028>',
-		cost: 7,
+		baseCost: 7,
 	},
 	mime: {
 		emote: '<:Mime:1342002399526654063>',
-		cost: 5,
+		baseCost: 5,
 	},
 	creditCard: {
 		emote: '<:Credit_Card:1342002441230483476>',
-		cost: 1,
+		baseCost: 1,
 	},
 	ceremonial_Dagger: {
 		emote: '<:Ceremonial_Dagger:1342002477750423582>',
-		cost: 6,
+		baseCost: 6,
 	},
 	banner: {
 		emote: '<:Banner:1342002566095179866>',
-		cost: 5,
+		baseCost: 5,
 	},
 	mysticSummit: {
 		emote: '<:Mystic_Summit:1342002885944414268>',
-		cost: 5,
+		baseCost: 5,
 	},
 	marbleJoker: {
 		emote: '<:Marble_Joker:1342003072389349429>',
-		cost: 6,
+		baseCost: 6,
 	},
 	loyaltyCard: {
 		emote: '<:Loyalty_Card:1342003162973737010>',
-		cost: 5,
+		baseCost: 5,
 	},
 	eightBall: {
 		emote: '<:8_Ball:1342006275353083904>',
-		cost: 5,
+		baseCost: 5,
 	},
 	misprint: {
 		emote: '<:Misprint:1342006756326506526>',
-		cost: 4,
+		baseCost: 4,
 	},
 	dusk: {
 		emote: '<:Dusk:1342006912920719401>',
-		cost: 5,
+		baseCost: 5,
 	},
 	raisedFist: {
 		emote: '<:Raised_Fist:1342015690403614727>',
-		cost: 5,
+		baseCost: 5,
 	},
 	chaosTheClown: {
 		emote: '<:Chaos_the_Clown:1342015885044482058>',
-		cost: 4,
+		baseCost: 4,
 	},
 	fibonacci: {
 		emote: '<:Fibonacci:1342016028791799809>',
-		cost: 8,
+		baseCost: 8,
 	},
 	steelJoker: {
 		emote: '<:Steel_Joker:1342016371592003675>',
-		cost: 7,
+		baseCost: 7,
 	},
 	scaryFace: {
 		emote: '<:Scary_Face:1342016710869258291>',
-		cost: 4,
+		baseCost: 4,
 	},
 	abstractJoker: {
 		emote: '<:Abstract_Joker:1342016871242797056>',
-		cost: 4,
+		baseCost: 4,
 	},
 	delayedGratification: {
 		emote: '<:Delayed_Gratification:1342017479177797682>',
-		cost: 4,
+		baseCost: 4,
 	},
 	hack: {
 		emote: '<:Hack:1342017569204207678>',
-		cost: 6,
+		baseCost: 6,
 	},
 	pareidolia: {
 		emote: '<:Pareidolia:1342018122047291413>',
-		cost: 5,
+		baseCost: 5,
 	},
 	grosMichel: {
 		emote: '<:Gros_Michel:1342018441028046932>',
-		cost: 5,
+		baseCost: 5,
 	},
 	evenSteven: {
 		emote: '<:Even_Steven:1342018494337908757>',
-		cost: 4,
+		baseCost: 4,
 	},
 	oddTodd: {
 		emote: '<:Odd_Todd:1342018579264049152>',
-		cost: 4,
+		baseCost: 4,
 	},
 	scholar: {
 		emote: '<:Scholar:1342020149963919401>',
-		cost: 4,
+		baseCost: 4,
 	},
 	businessCard: {
 		emote: '<:Business_Card:1342020506899058688>',
-		cost: 4,
+		baseCost: 4,
 	},
 	supernova: {
 		emote: '<:Supernova:1342026055682097193>',
-		cost: 5,
+		baseCost: 5,
 	},
 	rideTheBus: {
 		emote: '<:Ride_the_Bus:1342026189421936654>',
-		cost: 6,
+		baseCost: 6,
 	},
 	spaceJoker: {
 		emote: '<:Space_Joker:1342026308468871248>',
-		cost: 5,
+		baseCost: 5,
 	},
 	egg: {
 		emote: '<:Egg:1342026401531957291>',
-		cost: 4,
+		baseCost: 4,
 	},
 	burglar: {
 		emote: '<:Burglar:1342026487423041647>',
-		cost: 6,
+		baseCost: 6,
 	},
 	blackboard: {
 		emote: '<:Blackboard:1342026566355386378>',
-		cost: 6,
+		baseCost: 6,
 	},
 	runner: {
 		emote: '<:Runner:1342026844353990787>',
-		cost: 5,
+		baseCost: 5,
 	},
 	iceCream: {
 		emote: '<:Ice_Cream:1342026948691365909>',
-		cost: 5,
+		baseCost: 5,
 	},
 	dna: {
 		emote: '<:DNA:1342027493108088885>',
-		cost: 8,
+		baseCost: 8,
 	},
 	splash: {
 		emote: '<:Splash:1342027996042891264>',
-		cost: 3,
+		baseCost: 3,
 	},
 	blueJoker: {
 		emote: '<:Blue_Joker:1342028094613229659>',
-		cost: 5,
+		baseCost: 5,
 	},
 	sixthSense: {
 		emote: '<:Sixth_Sense:1342028342932672512>',
-		cost: 6,
+		baseCost: 6,
 	},
 	constellation: {
 		emote: '<:Constellation:1342028830990143539>',
-		cost: 6,
+		baseCost: 6,
 	},
 	hiker: {
 		emote: '<:Hiker:1342029660510228500>',
-		cost: 5,
+		baseCost: 5,
 	},
 	facelessJoker: {
 		emote: '<:Faceless_Joker:1342029738012835871>',
-		cost: 4,
+		baseCost: 4,
 	},
 	greenJoker: {
 		emote: '<:Green_Joker:1342029848469704745>',
-		cost: 4,
+		baseCost: 4,
 	},
 	superposition: {
 		emote: '<:Superposition:1342029866186309683>',
-		cost: 4,
+		baseCost: 4,
 	},
 	toDoList: {
 		emote: '<:To_Do_List:1342029911279276076>',
-		cost: 4,
+		baseCost: 4,
 	},
 	cavendish: {
 		emote: '<:Cavendish:1342029949644705792>',
-		cost: 4,
+		baseCost: 4,
 	},
 	cardSharp: {
 		emote: '<:Card_Sharp:1342030369909637171>',
-		cost: 6,
+		baseCost: 6,
 	},
 	redCard: {
 		emote: '<:Red_Card:1342031163929137183>',
-		cost: 5,
+		baseCost: 5,
 	},
 	madness: {
 		emote: '<:Madness:1342032557394493481>',
-		cost: 7,
+		baseCost: 7,
 	},
 	square_joker: {
 		emote: '<:Square_Joker:1342032643117809685>',
-		cost: 4,
+		baseCost: 4,
 	},
 	seance: {
 		emote: '<:Seance:1342032689846288406>',
-		cost: 6,
+		baseCost: 6,
 	},
 	riffRaff: {
 		emote: '<:Riff_Raff:1342032717893861426>',
-		cost: 6,
+		baseCost: 6,
 	},
 	vampire: {
 		emote: '<:Vampire:1342032737753632779>',
-		cost: 7,
+		baseCost: 7,
 	},
 	shortcut: {
 		emote: '<:Shortcut:1342032808025260113>',
-		cost: 7,
+		baseCost: 7,
 	},
 	hologram: {
 		emote: '<a:Hologram:1342034492344500245>',
-		cost: 7,
+		baseCost: 7,
 	},
 	vagabond: {
 		emote: '<:Vagabond:1342036462782382111>',
-		cost: 8,
+		baseCost: 8,
 	},
 	baron: {
 		emote: '<:Baron:1342041320197324802>',
-		cost: 8,
+		baseCost: 8,
 	},
 	cloudNine: {
 		emote: '<:Cloud_9:1342041356637573220>',
-		cost: 7,
+		baseCost: 7,
 	},
 	rocket: {
 		emote: '<:Rocket:1342041367303421992>',
-		cost: 6,
+		baseCost: 6,
 	},
 	obelisk: {
 		emote: '<:Obelisk:1342041376505860126>',
-		cost: 8,
+		baseCost: 8,
 	},
 	midasMask: {
 		emote: '<:Midas_Mask:1342041390577750057>',
-		cost: 7,
+		baseCost: 7,
 	},
 	luchador: {
 		emote: '<:Luchador:1342041401927405578>',
-		cost: 5,
+		baseCost: 5,
 	},
 	photograph: {
 		emote: '<:Photograph:1342041442335330345>',
-		cost: 5,
+		baseCost: 5,
 	},
 	giftCard: {
 		emote: '<:Gift_Card:1342042233993691179>',
-		cost: 6,
+		baseCost: 6,
 	},
 	turtleBean: {
 		emote: '<:TurtleBean:1342042279283789824>',
-		cost: 6,
+		baseCost: 6,
 	},
 	erosion: {
 		emote: '<:Erosion:1342042292277608511>',
-		cost: 6,
+		baseCost: 6,
 	},
 	reservedParking: {
 		emote: '<:Reserved_Parking:1342042309298094150>',
-		cost: 6,
+		baseCost: 6,
 	},
 	mailInRebate: {
 		emote: '<:Mail_In_Rebate:1342042512403202068>',
-		cost: 4,
+		baseCost: 4,
 	},
 	toTheMoon: {
 		emote: '<:To_the_Moon:1342042526219239514>',
-		cost: 5,
+		baseCost: 5,
 	},
 	hallucination: {
 		emote: '<:Hallucination:1342042545256927252>',
-		cost: 4,
+		baseCost: 4,
 	},
 	fortuneTeller: {
 		emote: '<:Fortune_Teller:1342042574247956480>',
-		cost: 6,
+		baseCost: 6,
 	},
 	juggler: {
 		emote: '<:Juggler:1342043125182500965>',
-		cost: 4,
+		baseCost: 4,
 	},
 	drunkard: {
 		emote: '<:Drunkard:1342043134691115080>',
-		cost: 4,
+		baseCost: 4,
 	},
 	stoneJoker: {
 		emote: '<:Stone_Joker:1342043194522865684>',
-		cost: 6,
+		baseCost: 6,
 	},
 	goldenJoker: {
 		emote: '<:Golden_Joker:1342043237174607958>',
-		cost: 6,
+		baseCost: 6,
 	},
 	luckyCat: {
 		emote: '<:Lucky_Cat:1342043621356212315>',
-		cost: 6,
+		baseCost: 6,
 	},
 	baseballCard: {
 		emote: '<:Baseball_Card:1342044001334857748>',
-		cost: 8,
+		baseCost: 8,
 	},
 	bull: {
 		emote: '<:Bull:1342044091877298218>',
-		cost: 6,
+		baseCost: 6,
 	},
 	dietCola: {
 		emote: '<:Diet_Cola:1342044341455163503>',
-		cost: 6,
+		baseCost: 6,
 	},
 	tradingCard: {
 		emote: '<:Trading_Card:1342044495704883232>',
-		cost: 6,
+		baseCost: 6,
 	},
 	flashCard: {
 		emote: '<:Flash_Card:1342044733500817479>',
-		cost: 5,
+		baseCost: 5,
 	},
 	popcorn: {
 		emote: '<:Popcorn:1342044750097682454>',
-		cost: 5,
+		baseCost: 5,
 	},
 	spareTrousers: {
 		emote: '<:Spare_Trousers:1342044765176332370>',
-		cost: 6,
+		baseCost: 6,
 	},
 	ancientJoker: {
 		emote: '<:Ancient_Joker:1342044946978439280>',
-		cost: 8,
+		baseCost: 8,
 	},
 	ramen: {
 		emote: '<:Ramen:1342044968344223797>',
-		cost: 6,
+		baseCost: 6,
 	},
 	walkieTalkie: {
 		emote: '<:Walkie_Talkie:1342044990104408114>',
-		cost: 4,
+		baseCost: 4,
 	},
 	seltzer: {
 		emote: '<:Seltzer:1342045000879570954>',
-		cost: 6,
+		baseCost: 6,
 	},
 	castle: {
 		emote: '<:Castle:1342045016985436161>',
-		cost: 6,
+		baseCost: 6,
 	},
 	smileyFace: {
 		emote: '<:Smiley_Face:1342045031443206154>',
-		cost: 4,
+		baseCost: 4,
 	},
 	campfire: {
 		emote: '<:Campfire:1342045089823719495>',
-		cost: 9,
+		baseCost: 9,
 	},
 	goldenTicket: {
 		emote: '<:Golden_Ticket:1342045221420269659>',
-		cost: 5,
+		baseCost: 5,
 	},
 	mrBones: {
 		emote: '<:Mr_Bones:1342045257658798170>',
-		cost: 5,
+		baseCost: 5,
 	},
 	acrobat: {
 		emote: '<:Acrobat:1342045284573904947>',
-		cost: 6,
+		baseCost: 6,
 	},
 	sockAndBuskin: {
 		emote: '<:Sock_and_Buskin:1342045298020581376>',
-		cost: 6,
+		baseCost: 6,
 	},
 	swashbuckler: {
 		emote: '<:Swashbuckler:1342045345399574562>',
-		cost: 4,
+		baseCost: 4,
 	},
 	troubadour: {
 		emote: '<:Troubadour:1342045377737654312>',
-		cost: 6,
+		baseCost: 6,
 	},
 	certificate: {
 		emote: '<:Certificate:1342045463683268609>',
-		cost: 6,
+		baseCost: 6,
 	},
 	smearedJoker: {
 		emote: '<:Smeared_Joker:1342045520012640257>',
-		cost: 7,
+		baseCost: 7,
 	},
 	throwback: {
 		emote: '<:Throwback:1342045553810473010>',
-		cost: 6,
+		baseCost: 6,
 	},
 	hangingChad: {
 		emote: '<:Hanging_Chad:1342045948855062528>',
-		cost: 4,
+		baseCost: 4,
 	},
 	roughGem: {
 		emote: '<:Rough_Gem:1342046101200572436>',
-		cost: 7,
+		baseCost: 7,
 	},
 	bloodstone: {
 		emote: '<:Bloodstone:1342046124730482749>',
-		cost: 7,
+		baseCost: 7,
 	},
 	arrowhead: {
 		emote: '<:Arrowhead:1342046136873123870>',
-		cost: 7,
+		baseCost: 7,
 	},
 	onyxAgate: {
 		emote: '<:Onyx_Agate:1342046146079621141>',
-		cost: 7,
+		baseCost: 7,
 	},
 	glassJoker: {
 		emote: '<:Glass_Joker:1342046367358648370>',
-		cost: 8,
+		baseCost: 8,
 	},
 	showman: {
 		emote: '<:Showman:1342046435717287966>',
-		cost: 5,
+		baseCost: 5,
 	},
 	flowerPot: {
 		emote: '<:Flower_Pot:1342046555854606377>',
-		cost: 6,
+		baseCost: 6,
 	},
 	blueprint: {
 		emote: '<:Blueprint:1342046568592969828>',
-		cost: 10,
+		baseCost: 10,
 	},
 	weeJoker: {
 		emote: '<:Wee_Joker:1342047775457673216>',
-		cost: 8,
+		baseCost: 8,
 	},
 	merryAndy: {
 		emote: '<:Merry_Andy:1342049128313786398>',
-		cost: 7,
+		baseCost: 7,
 	},
 	oopsAllSixes: {
 		emote: '<:Oops_All_6s:1342049172370620446>',
-		cost: 4,
+		baseCost: 4,
 	},
 	theIdol: {
 		emote: '<:The_Idol:1342049367321874453>',
-		cost: 6,
+		baseCost: 6,
 	},
 	seeingDouble: {
 		emote: '<:Seeing_Double:1342049576210796555>',
-		cost: 6,
+		baseCost: 6,
 	},
 	matador: {
 		emote: '<:Matador:1342049710130597961>',
-		cost: 7,
+		baseCost: 7,
 	},
 	hitTheRoad: {
 		emote: '<:Hit_the_Road:1342049745346101270>',
-		cost: 8,
+		baseCost: 8,
 	},
 	theDuo: {
 		emote: '<:The_Duo:1342049805274189895>',
-		cost: 8,
+		baseCost: 8,
 	},
 	theTrio: {
 		emote: '<:The_Trio:1342049857493536809>',
-		cost: 8,
+		baseCost: 8,
 	},
 	theFamily: {
 		emote: '<:The_Family:1342049873641603104>',
-		cost: 8,
+		baseCost: 8,
 	},
 	theOrder: {
 		emote: '<:The_Order:1342049886765580298>',
-		cost: 8,
+		baseCost: 8,
 	},
 	theTribe: {
 		emote: '<:The_Tribe:1342049895024038001>',
-		cost: 8,
+		baseCost: 8,
 	},
 	stuntman: {
 		emote: '<:Stuntman:1342050140172849273>',
-		cost: 7,
+		baseCost: 7,
 	},
 	invisibleJoker: {
 		emote: '<:Invisible_Joker:1342050202369916928>',
-		cost: 8,
+		baseCost: 8,
 	},
 	brainstorm: {
 		emote: '<:Brainstorm:1342050523959918624>',
-		cost: 10,
+		baseCost: 10,
 	},
 	satellite: {
 		emote: '<:Satellite:1342050579626852403>',
-		cost: 6,
+		baseCost: 6,
 	},
 	shootTheMoon: {
 		emote: '<:Shoot_The_Moon:1342050860242567228>',
-		cost: 5,
+		baseCost: 5,
 	},
 	driversLicense: {
 		emote: '<:Drivers_License:1342050943138533376>',
-		cost: 7,
+		baseCost: 7,
 	},
 	cartomancer: {
 		emote: '<:Cartomancer:1342050986792849438>',
-		cost: 6,
+		baseCost: 6,
 	},
 	astronomer: {
 		emote: '<:Astronomer:1342051009094225980>',
-		cost: 8,
+		baseCost: 8,
 	},
 	burntJoker: {
 		emote: '<:Burnt_Joker:1342051029893517393>',
-		cost: 8,
+		baseCost: 8,
 	},
 	bootstraps: {
 		emote: '<:Bootstraps:1342051069878079548>',
-		cost: 7,
+		baseCost: 7,
 	},
 	canio: {
 		emote: '<:Canio:1342051664449765441>',
-		cost: null,
+		baseCost: null,
 	},
 	triboulet: {
 		emote: '<:Triboulet:1342052024690278420>',
-		cost: null,
+		baseCost: null,
 	},
 	yorick: {
 		emote: '<:Yorick:1342052035494674485>',
-		cost: null,
+		baseCost: null,
 	},
 	chicot: {
 		emote: '<:Chicot:1342052046622294028>',
-		cost: null,
+		baseCost: null,
 	},
 	perkeo: {
 		emote: '<:Perkeo:1342052056311136388>',
-		cost: null,
+		baseCost: null,
 	},
 }
