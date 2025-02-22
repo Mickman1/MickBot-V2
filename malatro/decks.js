d3cbdad2903c0c43b3d3d0bc33c542060d9e13af
Mickman <70730433+Mickman1@users.noreply.github.com>
2025-02-22 03:39:37 -0500

    deck emotes

--- malatro/decks.js
+++ malatro/decks.js
@@ -1,40 +1,55 @@
 module.exports = {
 	red: {
 		discards: 3,
+		emote: '<:Red_Deck:1342774766435831870>',
 	},
 	blue: {
 		hands: 5,
+		emote: '<:Blue_Deck:1342774776258625536>',
 	},
 	yellow: {
 		money: 14,
+		emote: '<:Yellow_Deck:1342774785955987558>',
 	},
 	green: {
+		emote: '<:Green_Deck:1342774793702998057>',
 	},
 	black: {
 		jokerSlots: 6,
 		hands: 3,
+		emote: '<:Black_Deck:1342774802246533143>',
 	},
 	magic: {
+		emote: '<:Magic_Deck:1342775010716287016>',
 	},
 	nebula: {
 		consumableSlots: 1,
+		emote: '<:Nebula_Deck:1342775034476892191>',
 	},
 	ghost: {
+		emote: '<:Ghost_Deck:1342775049467330590>',
 	},
 	abandoned: {
+		emote: '<:Abandoned_Deck:1342775061328957480>',
 	},
 	checkered: {
+		emote: '<:Checkered_Deck:1342775073572130848>',
 	},
 	zodiac: {
+		emote: '<:Zodiac_Deck:1342775087690023013>',
 	},
 	painted: {
 		handSize: 10,
 		jokerSlots: 4,
+		emote: '<:Painted_Deck:1342775098376978454>',
 	},
 	anaglyph: {
+		emote: '<:Anaglyph_Deck:1342775113287991376>',
 	},
 	plasma: {
+		emote: '<:Plasma_Deck:1342775159118889032>',
 	},
 	erratic: {
+		emote: '<:Erratic_Deck:1342775170829652039>',
 	},
 }
