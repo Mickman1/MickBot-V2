fbeaeeb9e767a2b7af4b2e0bdd00ceb9e5e4804c
Mickman <70730433+Mickman1@users.noreply.github.com>
2025-02-22 21:00:09 -0500

    planets, hand base chips & mult

---
+++ malatro/planets.js
@@ -0,0 +1,62 @@
+module.exports = {
+	pluto: {
+		hand: 'highCard',
+		additionChips: 10,
+		additionMult: 1,
+	},
+	mercury: {
+		hand: 'pair',
+		additionChips: 15,
+		additionMult: 1,
+	},
+	uranus: {
+		hand: 'twoPair',
+		additionChips: 20,
+		additionMult: 1,
+	},
+	venus: {
+		hand: 'threeOfAKind',
+		additionChips: 20,
+		additionMult: 2,
+	},
+	saturn: {
+		hand: 'straight',
+		additionChips: 30,
+		additionMult: 3,
+	},
+	jupiter: {
+		hand: 'flush',
+		additionChips: 15,
+		additionMult: 2,
+	},
+	earth: {
+		hand: 'fullHouse',
+		additionChips: 25,
+		additionMult: 2,
+	},
+	mars: {
+		hand: 'fourOfAKind',
+		additionChips: 30,
+		additionMult: 3,
+	},
+	neptune: {
+		hand: 'straightFlush',
+		additionChips: 40,
+		additionMult: 4,
+	},
+	planetX: {
+		hand: 'fiveOfAKind',
+		additionChips: 35,
+		additionMult: 3,
+	},
+	ceres: {
+		hand: 'flushHouse',
+		additionChips: 40,
+		additionMult: 4,
+	},
+	eris: {
+		hand: 'flushFive',
+		additionChips: 50,
+		additionMult: 3,
+	},
+}
